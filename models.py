26a38a21e42c959e4e239438db787df2cc927f51
HenryPaik1 <renwenxue0@gmail.com>
2019-06-13 12:13:25 +0900

    add svd_stl_ets model

--- models.py
+++ models.py
@@ -12,6 +12,79 @@ from dateutil.relativedelta import relativedelta
 from dateutil import rrule
 from utils import *
 
+def svd_stl_ets(data, data_test, dept, seasonal='add'):
+    name = 'pca_stl_ets'
+    
+    pca_data = pca_decomposition(data, dept)
+    idx = pca_data.columns
+    condition = data_test['Dept'] == dept
+
+    for store in data_test[condition]['Store'].unique():
+        try:
+            print('predict store:', store)
+            fcst_len = get_fcst_len(store, dept, data, data_test)
+            ts = pca_data.loc[:, store]
+            
+            # seasonal decompose
+            seasonal_obj = seasonal_decompose(ts, freq=52)
+            seasonality = seasonal_obj.seasonal
+
+            # seasonal adjust
+            seasonal_adjusted = ts - seasonality
+            
+            # fit and fcst
+            fit = ExponentialSmoothing(seasonal_adjustment, seasonal=seasonal, seasonal_periods=52, trend=None).fit(optimized=True, remove_bias=True)
+
+            fcst_len = get_fcst_len(data=df, store=store, dept=dept, data_test=data_test)
+            fcst = fit.forecast(fcst_len)
+            fcst_df = pd.DataFrame(fcst)
+            fcst_df['Store'] = store
+            fcst_df['Dept'] = dept
+            fcst_df = fcst_df.rename(columns={0:'Weekly_Sales'})
+            
+            # re-seasonalize
+            temp = fcst_df.reset_index().assign(week_num=lambda x: x['index'].dt.week).merge(df_seasonality, on='week_num')
+            fcst_df.loc[:,'Weekly_Sales'] = temp.apply(lambda x: x['Weekly_Sales_x'] + x['Weekly_Sales_y'], axis=1).values
+            fcst_df = fcst_df.reset_index().rename(columns={'index':'Date'})
+
+            #send_message(store, name, store=True, fail=False)
+            yield fcst_df
+        
+        except:
+            print('    fail store {} '.format(store))
+            # slack
+            send_message(store, name, store=True, fail=True)
+            pass
+        
+def run_svd_stl_ets(df, test):
+    name = 'pca_stl_ets'
+    
+    ans = pd.DataFrame()
+    dept_ls = list(set([tag.split('_')[1] for tag in test['check'].unique()]))
+    dept_ls = list(map(int, dept_ls))
+
+    for dept in dept_ls:
+        i = 0
+        print('predict dept: {}'.format(dept))
+        
+        # slack
+        text = 'predict dept {} start'.format(dept)
+        send_text(name, text)
+        
+        for fcst_yield_df in svd_stl_ets(df, test, dept):
+            i += 1
+            ans = ans.append(fcst_yield_df, ignore_index=True)
+            ans.to_csv('complete_svd_ets.csv', index=False)
+            print('    saved: ', i)
+            
+            filled = len(ans)
+            text = 'current filled: {}'.format(filled)
+            send_text(name, text)
+
+        #slack
+        send_message(dept, name, store=False, fail=False)
+    return ans        
+
 def pca_stl_sarima(df, data_test, dept):
     
     name = 'pca_stl_sarima'
